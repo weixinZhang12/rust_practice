761964cd6d72dfa694277df86410c09b2facdfe4
DreamYa <132214306+weixinZhang12@users.noreply.github.com>
2024-11-02 16:48:56 +0800

    Delete target directory

--- target/debug/rust_practice.d
+++
@@ -1,1 +0,0 @@
-/home/dreamya/桌面/rust_practice/target/debug/rust_practice: /home/dreamya/桌面/rust_practice/src/main.rs /home/dreamya/桌面/rust_practice/src/p38.rs /home/dreamya/桌面/rust_practice/src/p39.rs

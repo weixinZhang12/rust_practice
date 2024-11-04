1e2b9360312f3f53c21915eec095b6c2806ee917
k7519586.*Z <z2625687838@gmail.com>
2024-11-04 09:02:26 +0800

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-use leetcode::  l20;
+use leetcode::l20;
 
-mod leetcode{
-     pub  mod l7;
-     pub mod l1;
-     pub mod l13;
-     pub mod l14;
-     pub mod l20;
+mod leetcode {
+    pub mod l1;
+    pub mod l13;
+    pub mod l14;
+    pub mod l20;
+    pub mod l7;
 }
 fn main() {
     // println!("Hello, world!");
-    let result=l20::is_vaild("()(){}{".to_string());
-    println!("{:?}",result);
-
+    let result = l20::is_vaild("()(){}{".to_string());
+    println!("{:?}", result);
 }
